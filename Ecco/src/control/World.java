f130be59838b1d1b36374493e8b678da22636297
haonmde130144 <haonmde130144@fpt.edu.vn>
2019-01-25 15:09:02 +0700

    second

--- Ecco/src/control/World.java
+++ Ecco/src/control/World.java
@@ -1,5 +1,24 @@
 package control;
-
+import java.util.*;
 public class World {
-
+  public int nRow;
+  public int nColumn;
+  private double[][] cells;
+  public World(int row , int column) {
+	  nRow =row;
+	  nColumn = column;
+	  Random rd = new Random();
+	  cells = new double[row][column];
+	  for(int i=0 ; i<row;i++) {
+		  for(int j=0;j<column;j++) {
+			  cells[i][j]= rd.nextDouble();
+		  }
+	  }
+	  
+  }
+  public double getGrass(int i, int j) {
+	  return cells[i][j];
+     
+  }
+  
 }
